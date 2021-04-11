019d6321939fb778fe4e4987fc5966df26d0f495
andijayawizard <andijayawizard@gmail.com>
2021-04-11 21:49:05 +0700

    1104

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   HOST: "localhost",
-  USER: "root",
-  PASSWORD: "123456",
-  DB: "testdb",
+  USER: "bezkoder",
+  PASSWORD: "bezkoder",
+  DB: "bezkoder",
   dialect: "mysql",
   pool: {
     max: 5,
